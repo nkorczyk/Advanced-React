918e5d883e626676d9ba4b172cbaebc4bf436647
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-08-09 16:06:55 +0200

    Added editorconfig

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 
 const Home = () => (
-    <div>
-        <p>Hey!</p>
-        <Link href="/sell">
-            <a>Sell!</a>
-        </Link>
-    </div>
+  <div>
+    <p>Hey!</p>
+    <Link href="/sell">
+      <a>Sell!</a>
+    </Link>
+  </div>
 );
 
 export default Home;
